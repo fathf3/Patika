9a24c46a27a83c487a3212f9ba3b89f0b1f0c3f1
fathf3 <fatih-348@hotmail.com>
2023-07-31 17:15:48 +0300

    Patika Net Core

---
+++ Fibonacci.cs
@@ -0,0 +1,17 @@
+﻿
+static int Fibo(int number)
+{
+    if(number == 0) return 0;
+    else if(number == 1) return 1;  
+    return Fibo(number-1)+Fibo(number-2);
+}
+
+static float Average(int number)
+{
+    return Fibo(number)/number;
+}
+
+
+Console.WriteLine(Average(14));
+
+    
